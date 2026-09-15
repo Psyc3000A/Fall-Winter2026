813c305c213b4fdd2a7cc7b5551fb7aa481d91a7
q-cbm <310819634+q-cbm@users.noreply.github.com>
2026-09-15 18:37:31 -0400

    Add comment for pandas installation in Jupyter
    Added a comment for installing pandas in Jupyter.

--- code1.py
+++ code1.py
@@ -1,3 +1,4 @@
+#!mamba install pandas  #uncomment when in Jupyter
 import pandas as pd
 datafile=pd.read_csv ("https://raw.githubusercontent.com/Psyc3000A/Data/refs/heads/main/sleep.csv")
 datafile.groupby('group').describe()
